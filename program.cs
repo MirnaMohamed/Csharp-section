2a25a9768b669a1bc5b3830d71fac8dd2d7eedf5
MirnaMohamed <45853143+MirnaMohamed@users.noreply.github.com>
2022-12-13 20:21:05 +0200

    Create Person Class

--- program.cs
+++ program.cs
@@ -1,7 +1,18 @@
 using System;
 namespace Program;
+
+public abstract class Person {
+	string name;
+	int age ;
+	public Person (string n, int a){
+		name = n;
+		age = a;
+	}
+	public abstract void Print();
+} 
+
 public class MainProgram {
 	public static void Main(){
-		Console.WriteLine("The program is running successfully");
+		
 	}
 }
