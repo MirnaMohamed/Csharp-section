268811e85ba087ffba90fd780c4038ffe88f2b43
MirnaMohamed <45853143+MirnaMohamed@users.noreply.github.com>
2022-12-13 20:50:03 +0200

    Create People class

--- program.cs
+++ program.cs
@@ -2,17 +2,54 @@ using System;
 namespace Program;
 
 public abstract class Person {
-	string name;
-	int age ;
-	public Person (string n, int a){
-		name = n;
+	public string Name;
+	public int age ;
+	public Person (string name, int a){
+		Name = n;
 		age = a;
 	}
 	public abstract void Print();
 } 
+public class Student : Person{
+	public int Year ;
+	public float GPA ;
+	public Student (string n, int a, int y, int g){
+		Name=n;
+		age =a;
+		Year = y;
+		GPA =g;
+	}
+	public override void Print(){
+		Console.WriteLine($"My name is {0} and my GPA is {1}", name, GPA);
+	}
+}
+
+public class Database {
+	private int currentIndex ;
+	public Person[] People = new Person[50];
+	public void AddStudent(Student student){
+		People[currentIndex++] = student;
+	}
+}
+
+public class Staff: Person {
+	public double Salary;
+	public int JoinYear;
+}
 
 public class MainProgram {
 	public static void Main(){
-		
+		var database = new Database();
+		Console.Write("Name: ");
+		var name = Console.ReadLine();
+		Console.Write("Age: ");
+		var age = Convert.ToInt32(Console.ReadLine());
+		Console.Write("Year: ");
+		var year = Convert.ToInt32(Console.ReadLine());
+		Console.Write("GPA: ");
+		var gpa = Convert.ToInt32(Console.ReadLine());
+		var student = new Student(name, age, year, gpa);
+		database.AddStudent(student);
+		Console.WriteLine("The student is added successfully");
 	}
 }
