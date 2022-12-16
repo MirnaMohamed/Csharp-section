fb165bffa73df512aa5f92a3af6fc4b9e22cec22
MirnaMohamed <45853143+MirnaMohamed@users.noreply.github.com>
2022-12-16 15:21:50 +0200

    Task 3

--- program.cs
+++ program.cs
@@ -1,14 +1,16 @@
 using System;
 namespace Program;
 
-public abstract class Person {
+public class Person {
 	public string Name;
 	public int Age ;
 	public Person (string name, int age){
 		Name = name;
 		Age = age;
 	}
-	public abstract void Print();
+	public virtual void Print(){
+		Console.WriteLine("My name is {0} and my age is {1}", Name, Age);
+	}
 } 
 public class Student : Person{
 	public int Year ;
@@ -19,6 +21,18 @@ public class Student : Person{
 	}
 	public override void Print(){
 		Console.WriteLine("My name is {0} and my GPA is {1}", Name, GPA);
+	}
+}
+
+public class Staff: Person {
+	public double Salary;
+	public int JoinYear;
+	public Staff (string name, int age, double salary, int jyear): base(name, age){
+		Salary = salary;
+		JoinYear = jyear;
+	}
+	public override void Print(){
+		Console.WriteLine("My name is {0}, my age is {1}, and my salary is {2}", Name, Age, Salary);
 	}
 }
 
@@ -35,24 +49,15 @@ public class Database {
 		for(int i=0; i< currentIndex; i++){
 			People[i].Print();
 	}	}
+	public void AddPerson(Person person){
+		People[currentIndex++] = person;
 	}
-
-public class Staff: Person {
-	public double Salary;
-	public int JoinYear;
-	public Staff (string name, int age, double salary, int jyear): base(name, age){
-		Salary = salary;
-		JoinYear = jyear;
 	}
-	public override void Print(){
-		Console.WriteLine("My name is {0}, my age is {1}, and my salary is {2}", Name, Age, Salary);
-	}
-}
 
 public class MainProgram {
 	public static void Main(){
 		var database = new Database();
-		Console.WriteLine("Choose 1 for  student, 2 for staff, 3 for printing all people");
+		Console.WriteLine("Choose 0 to stop, 1 for  student, 2 for staff, 3 for printing all people, else for any kind of people");
 		int x = Convert.ToInt32(Console.ReadLine());
 		string name; int age;
 		while(true){
@@ -81,10 +86,17 @@ public class MainProgram {
 				break;
 				case 3: database.PrintAll();
 				break;
-				default: return;
+				case 0: return;
+				default: Console.Write("Name: ");
+				name = Console.ReadLine();
+				Console.Write("Age: ");
+				age = Convert.ToInt32(Console.ReadLine());
+				var person = new Person(name, age);
+				database.AddPerson(person);
+				break;
 			} 
-		Console.WriteLine("Choose 1 for  student, 2 for staff, 3 for printing all people");
-		x = Convert.ToInt32(Console.ReadLine());
+		Console.WriteLine("Choose 0 to stop, 1 for  student, 2 for staff, 3 for printing all people");
+		 x = Convert.ToInt32(Console.ReadLine());
 		}
 	}
-} 
+}
